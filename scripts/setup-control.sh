4ed04c3c7952b3328aa18fc726ec4843182ba783
yagami <yagami19972024@hotmail.com>
2025-07-23 23:48:04 -0500

    Sync

--- scripts/setup-control.sh
+++ scripts/setup-control.sh
@@ -23,7 +23,7 @@ chmod 700 /home/ansible/.ssh
 echo "[CONTROL] Generate SSH Key for Ansible:"
 sudo -u ansible ssh-keygen -t rsa -b 4096 -f /home/ansible/.ssh/id_rsa -N ""
 
-echo "[NODE] Enable password authentication:"
+echo "[CONTROL] Enable password authentication:"
 sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config.d/60-cloudimg-settings.conf || true
 sed -i 's/^#PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
@@ -31,7 +31,8 @@ sudo systemctl restart ssh
 
 echo "[CONTROL] Copy SSH Key to Managed Nodes:"
 for ip in 192.168.56.11 192.168.56.12; do
-    sshpass -p "ansible" ssh-copy-id -o StrictHostKeyChecking=no ansible@$ip
+    sshpass -p "ansible" scp ansible@control /home/ansible/.ssh/id_rsa.pub ansible@$ip:/home/ansible/.ssh/ansible.pub
+    sshpass -p "ansible" ssh -o StrictHostKeyChecking=no ansible@$ip "cat ~/.ssh/ansible.pub >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys && rm -fv ~/.ssh/ansible.pub"
 done
 
 echo "[CONTROL] Add managed nodes to /etc/hosts:"
