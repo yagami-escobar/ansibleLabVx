dda339ed80fa8ccd2daf5a2021dc188874e5e149
yagami <yagami19972024@hotmail.com>
2025-07-23 23:27:21 -0500

    Sync

--- scripts/setup-control.sh
+++ scripts/setup-control.sh
@@ -6,31 +6,33 @@ apt-get update
 apt-get install -y sudo vim python3 python3-pip openssh-client sshpass ansible curl iputils-ping net-tools iproute2 tree git
 
 
-echo "[CONTROL] Create Ansible User:"
+echo "[CONTROL] Create && Config User(Ansible):"
 useradd -m ansible
 echo "ansible:ansible" | chpasswd
 usermod -aG sudo ansible
 mkdir -p /home/ansible
+chsh -s /bin/bash ansible
+echo 'ansible ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
 
 echo "[CONTROL] Config SSH Home:"
 mkdir -p /home/ansible/.ssh
 chown -R ansible:ansible /home/ansible/.ssh
 chmod 700 /home/ansible/.ssh
-echo 'ansible ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
 
 
 echo "[CONTROL] Generate SSH Key for Ansible:"
 sudo -u ansible ssh-keygen -t rsa -b 4096 -f /home/ansible/.ssh/id_rsa -N ""
 
 echo "[NODE] Enable password authentication:"
+sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config.d/60-cloudimg-settings.conf || true
 sed -i 's/^#PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-systemctl restart ssh
+sudo systemctl restart ssh
 
-# echo "[CONTROL] Copy SSH Key to Managed Nodes:"
-# for ip in 192.168.56.11 192.168.56.12; do
-#     sshpass -p "ansible" ssh-copy-id -o StrictHostKeyChecking=no ansible@$ip
-# done
+echo "[CONTROL] Copy SSH Key to Managed Nodes:"
+for ip in 192.168.56.11 192.168.56.12; do
+    sshpass -p "ansible" ssh-copy-id -o StrictHostKeyChecking=no ansible@$ip
+done
 
 echo "[CONTROL] Add managed nodes to /etc/hosts:"
 cat<<EOF >> /etc/hosts
--- scripts/setup-node.sh
+++ scripts/setup-node.sh
@@ -10,6 +10,9 @@ echo "[NODE] Create Ansible User"
 useradd -m ansible
 echo "ansible:ansible" | chpasswd
 usermod -aG sudo ansible
+mkdir -p /home/ansible
+chsh -s /bin/bash ansible
+
 
 echo "[NODE] Config SSH Home:"
 mkdir -p /var/run/sshd
@@ -18,13 +21,14 @@ touch /home/ansible/.ssh/authorized_keys
 chown -R ansible:ansible /home/ansible/.ssh
 chmod 700 /home/ansible/.ssh
 chmod 600 /home/ansible/.ssh/authorized_keys
-chsh -s /bin/bash ansible
+
 echo 'ansible ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
 
 echo "[NODE] Enable password authentication:"
+sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config.d/60-cloudimg-settings.conf || true
 sed -i 's/^#PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-systemctl restart ssh
+sudo systemctl restart ssh
 
 echo "[NODE] Add control to /etc/hosts"
 cat<<EOF >> /etc/hosts
