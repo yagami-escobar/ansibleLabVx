2cfe4798fa716ee3eb5bd15812586017813f861c
yagami <yagami19972024@hotmail.com>
2025-07-24 09:50:14 -0500

    Sync

--- scripts/setup-control.sh
+++ scripts/setup-control.sh
@@ -42,3 +42,67 @@ for node in "${NODES[@]}"; do
     sshpass -p "ansible" ssh-copy-id -o StrictHostKeyChecking=no -i /home/ansible/.ssh/id_rsa.pub ansible@$node
 done
 # "
+
+#! /bin/bash
+set -euo pipefail
+
+echo "[CONTROL] Install Pkgs Base:"
+apt-get update
+apt-get install -y sudo vim python3 python3-pip openssh-client sshpass ansible curl iputils-ping net-tools iproute2 tree git apt-transport-https ca-certificates gnupg lsb-release
+
+echo "[CONTROL] Create && Config User(Ansible):"
+useradd -m ansible
+echo "ansible:ansible" | chpasswd
+usermod -aG sudo ansible
+mkdir -p /home/ansible
+chsh -s /bin/bash ansible
+echo 'ansible ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+
+echo "[CONTROL] Config SSH Home:"
+mkdir -p /home/ansible/.ssh
+chown -R ansible:ansible /home/ansible/.ssh
+chmod 700 /home/ansible/.ssh
+
+echo "[CONTROL] Generate SSH Key for Ansible:"
+sudo -u ansible ssh-keygen -t rsa -b 4096 -f /home/ansible/.ssh/id_rsa -N ""
+
+echo "[CONTROL] Enable password authentication:"
+sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config.d/60-cloudimg-settings.conf || true
+sed -i 's/^#PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sudo systemctl restart ssh
+
+echo "[CONTROL] Add managed nodes to /etc/hosts:"
+cat<<EOF >> /etc/hosts
+192.168.56.11 node1
+192.168.56.12 node2
+EOF
+
+
+echo "[CONTROL] Copy SSH Key to Managed Nodes:"
+NODES=(node1 node2)
+for node in "${NODES[@]}"; do
+    sshpass -p "ansible" ssh-copy-id -o StrictHostKeyChecking=no -i /home/ansible/.ssh/id_rsa.pub ansible@$node
+done
+
+# -------------------------------
+# DOCKER ENGINE
+# -------------------------------
+
+echo "[CONTROL] Install Docker Engine:"
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+  gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+ARCH=$(dpkg --print-architecture)
+RELEASE=$(lsb_release -cs)
+
+echo \
+  "deb [arch=$ARCH signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $RELEASE stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+usermod -aG docker ansible
+g
